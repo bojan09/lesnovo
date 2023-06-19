79937b8512f26e4a676293648e3f4a293568e06e
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-19 20:11:05 +0200

    updates in About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,15 +7,15 @@ import img_3 from "../assets/cave/cave_02.jpg";
 
 const About = () => {
   return (
-    <div className="text-center">
-      <h1 className="primary-heading text-3xl font-semibold  mb-[5rem]">
+    <div className="text-center  h-[100vh]">
+      <h1 className="primary-heading text-3xl font-semibold my-[2rem]">
         EXCITING TOURS FOR ADVENTUROUS PEOPLE
       </h1>
 
       {/* Container */}
-      <div className="flex justify-evenly gap-6 p-3 py-[3rem]">
+      <div className=" flex justify-around gap-[6rem] p-3 py-[3rem]">
         {/* Left Container */}
-        <div className="w-[50%] text-start flex flex-col justify-center items-center gap-12">
+        <div className="w-[40%] text-start flex flex-col justify-center items-center gap-12">
           <div className="w-[50ch] ">
             <h1 className="mb-6">YOU'RE GOING TO FALL IN LOVE WITH NATURE</h1>
             <p>
@@ -32,8 +32,8 @@ const About = () => {
               quam nisi exercitationem omnis earum qui.
             </p>
           </div>
-          <button>
-            <a href="#contact">Learn more</a>
+          <button className="border-b-2 border-primaryColor px-2 py-1 hover:bg-primaryColor transition-all duration-200 ease-in hover:text-white scale-100 hover:scale-105">
+            <a href="#contact">Learn more →</a>
           </button>
         </div>
 
