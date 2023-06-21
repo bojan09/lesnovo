2824346f604323fd0ab3bc7267d181b3c728b2bd
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-21 14:52:17 +0200

    refactoring Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import "./Navbar.css";
 import React, { useState } from "react";
 
 // assets
@@ -9,57 +8,47 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <div className="navigation">
-      <input
-        type="checkbox"
-        className="navigation__checkbox"
-        id="navi-toggle"
+    <div className="">
+      <img
+        src={!toggle ? menu : close}
+        className="w-[45px] h-[45px] object-contain cursor-pointer fixed right-4 top-2 "
+        alt="nav menu"
+        onClick={() => setToggle(!toggle)}
       />
 
-      <label
-        for="navi-toggle"
-        className="navigation__button flex justify-center items-center"
+      <ul
+        className={
+          toggle
+            ? "flex flex-col justify-center items-center gap-10 bg-red-200 h-[100vh] w-[100vw] z-10"
+            : "hidden"
+        }
       >
-        {/* <span className="navigation__icon">&nbsp;</span> */}
-        <img
-          src={!toggle ? menu : close}
-          className="w-[60%] h-[60%] object-contain cursor-pointer "
-          alt="nav menu"
-          onClick={() => setToggle(!toggle)}
-        />
-      </label>
-
-      <div className="navigation__background">&nbsp;</div>
-
-      <nav className="navigation__nav">
-        <ul className="navigation__list">
-          <li className="navigation__item" onClick={() => setToggle(!toggle)}>
-            <a href="#about" className="navigation__link">
-              <span>01</span>About Natous
-            </a>
-          </li>
-          <li className="navigation__item" onClick={() => setToggle(!toggle)}>
-            <a href="#features" className="navigation__link">
-              <span>02</span>Your benfits
-            </a>
-          </li>
-          <li className="navigation__item" onClick={() => setToggle(!toggle)}>
-            <a href="#tours" className="navigation__link">
-              <span>03</span>Popular tours
-            </a>
-          </li>
-          <li className="navigation__item" onClick={() => setToggle(!toggle)}>
-            <a href="#testimonials" className="navigation__link">
-              <span>04</span>Stories
-            </a>
-          </li>
-          <li className="navigation__item" onClick={() => setToggle(!toggle)}>
-            <a href="#contact" className="navigation__link">
-              <span>05</span>Book now
-            </a>
-          </li>
-        </ul>
-      </nav>
+        <li className="" onClick={() => setToggle(!toggle)}>
+          <a href="#about" className="">
+            <span>01</span>About Natous
+          </a>
+        </li>
+        <li className="" onClick={() => setToggle(!toggle)}>
+          <a href="#features" className="">
+            <span>02</span>Your benfits
+          </a>
+        </li>
+        <li className="" onClick={() => setToggle(!toggle)}>
+          <a href="#tours" className="">
+            <span>03</span>Popular tours
+          </a>
+        </li>
+        <li className="" onClick={() => setToggle(!toggle)}>
+          <a href="#testimonials" className="">
+            <span>04</span>Stories
+          </a>
+        </li>
+        <li className="" onClick={() => setToggle(!toggle)}>
+          <a href="#contact" className="">
+            <span>05</span>Book now
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,63 +1,7 @@
 import React from "react";
 
-// assets
-import img_1 from "../assets/cave/cave_01.jpg";
-import img_2 from "../assets/cave/cave_3.jpg";
-import img_3 from "../assets/cave/cave_02.jpg";
-
 const About = () => {
-  return (
-    <div className="text-center  h-[100vh]">
-      <h1 className="primary-heading text-3xl font-semibold my-[2rem]">
-        EXCITING TOURS FOR ADVENTUROUS PEOPLE
-      </h1>
-
-      {/* Container */}
-      <div className=" flex justify-around gap-[6rem] p-3 py-[3rem]">
-        {/* Left Container */}
-        <div className="w-[40%] text-start flex flex-col justify-center items-center gap-12">
-          <div className="w-[50ch] ">
-            <h1 className="mb-6">YOU'RE GOING TO FALL IN LOVE WITH NATURE</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
-              ipsum sapiente aspernatur libero repellat quis consequatur ducimus
-              quam nisi exercitationem omnis earum qui.
-            </p>
-          </div>
-          <div className="w-[50ch]">
-            <h1 className="mb-6">LIVE ADVENTURES LIKE YOU NEVER HAVE BEFORE</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
-              ipsum sapiente aspernatur libero repellat quis consequatur ducimus
-              quam nisi exercitationem omnis earum qui.
-            </p>
-          </div>
-          <button className="border-b-2 border-primaryColor px-2 py-1 hover:bg-primaryColor transition-all duration-200 ease-in hover:text-white scale-100 hover:scale-105">
-            <a href="#contact">Learn more →</a>
-          </button>
-        </div>
-
-        {/* Right Container */}
-        <div className="w-[50%] relative">
-          <img
-            src={img_1}
-            alt="img_1"
-            className="w-[500px]  h-[350px] object-cover absolute top-0 left-[-10rem] rounded-lg hover:z-10 scale-100 hover:scale-105 transition-all duration-500 ease-in-out"
-          />
-          <img
-            src={img_2}
-            alt="img_2"
-            className="w-[500px] h-[350px] object-cover absolute top-10 left-[15rem] rounded-lg hover:z-10 scale-100 hover:scale-105 transition-all duration-200 ease-in-out"
-          />
-          <img
-            src={img_3}
-            alt="img_3"
-            className="w-[500px] h-[350px] object-cover absolute top-[20rem] left-[5rem] rounded-lg hover:z-10 scale-100 hover:scale-105 transition-all duration-200 ease-in-out"
-          />
-        </div>
-      </div>
-    </div>
-  );
+  return <div>About</div>;
 };
 
 export default About;
