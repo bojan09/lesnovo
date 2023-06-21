759ae10fe476da7d427a5d22e2217ed3213fff39
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-21 22:15:37 +0200

    adding Features page

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -7,50 +7,50 @@ import heart from "../assets/features/heart.png";
 
 const Features = () => {
   return (
-    <div className="features py-[5rem] my-[5rem]">
-      <div className="skew-y-[7deg]  md:flex justify-center items-center">
-        <div className="text-center py-3 px-4">
-          <img src={globe} alt="globe" className="mx-auto my-2" />
-          <h1 className="my-2 text-[#f9f9f9] text-lg font-semibold">
+    <div className="features py-[5rem] sm:py-[10rem] my-[5rem]">
+      <div className="skew-y-[7deg]  md:flex justify-center items-center gap-6 flex-wrap">
+        <div className="mx-auto xs:my-4 sm:m-0 rounded-lg max-w-[20rem] max-h-[22rem] text-center py-3 px-4 bg-white opacity-80 scale-100 hover:scale-105 transition-all duration-200 ease-in-out">
+          <img src={globe} alt="globe" className="mx-auto my-2 w-[50px]" />
+          <h1 className="mt-12 text-[#333] text-lg font-semibold">
             EXPLORE THE WORLD
           </h1>
-          <p className="text-lg text-[#f9f9f9]">
+          <p className="text-lg text-[#333] pb-[3rem] pt-4">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
             voluptate at minima neque doloribus saepe nulla quo ipsam numquam
             enim.
           </p>
         </div>
 
-        <div className="text-center py-3 px-4">
-          <img src={compas} alt="globe" className="mx-auto my-2" />
-          <h1 className="my-2 text-[#f9f9f9] text-lg font-semibold">
+        <div className="mx-auto xs:my-4 sm:m-0 rounded-lg max-w-[20rem] max-h-[22rem] text-center py-3 px-4 bg-white opacity-80 scale-100 hover:scale-105 transition-all duration-200 ease-in-out">
+          <img src={compas} alt="globe" className="mx-auto my-2 w-[50px]" />
+          <h1 className="mt-12 text-[#333] text-lg font-semibold">
             MEET NATURE
           </h1>
-          <p className="text-lg text-[#f9f9f9]">
+          <p className="text-lg text-[#333] pb-[3rem] pt-4">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
             voluptate at minima neque doloribus saepe nulla quo ipsam numquam
             enim.
           </p>
         </div>
 
-        <div className="text-center py-3 px-4">
-          <img src={map} alt="globe" className="mx-auto my-2" />
-          <h1 className="my-2 text-[#f9f9f9] text-lg font-semibold">
+        <div className="mx-auto xs:my-4 sm:m-0 rounded-lg max-w-[20rem] max-h-[22rem] text-center py-3 px-4 bg-white opacity-80 scale-100 hover:scale-105 transition-all duration-200 ease-in-out">
+          <img src={map} alt="globe" className="mx-auto my-2 w-[50px]" />
+          <h1 className="mt-12 text-[#333] text-lg font-semibold">
             FIND YOUR WAY
           </h1>
-          <p className="text-lg text-[#f9f9f9]">
+          <p className="text-lg text-[#333] pb-[3rem] pt-4">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
             voluptate at minima neque doloribus saepe nulla quo ipsam numquam
             enim.
           </p>
         </div>
 
-        <div className="text-center py-3 px-4">
-          <img src={heart} alt="globe" className="mx-auto my-2" />
-          <h1 className="my-2 text-[#f9f9f9] text-lg font-semibold">
+        <div className="mx-auto xs:my-4 sm:m-0 rounded-lg max-w-[20rem] max-h-[22rem] text-center py-3 px-4 bg-white opacity-80 scale-100 hover:scale-105 transition-all duration-200 ease-in-out">
+          <img src={heart} alt="globe" className="mx-auto my-2 w-[50px]" />
+          <h1 className="mt-12 text-[#333] text-lg font-semibold">
             LIVE A HEALTHIER LIFE
           </h1>
-          <p className="text-lg text-[#f9f9f9]">
+          <p className="text-lg text-[#333] pb-[3rem] pt-4">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
             voluptate at minima neque doloribus saepe nulla quo ipsam numquam
             enim.
