7365128b30175ba337d5e95ba4a244a62495c5b8
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-21 15:48:29 +0200

    refactored Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     <div className="">
       <img
         src={!toggle ? menu : close}
-        className="w-[45px] h-[45px] object-contain cursor-pointer fixed right-4 top-2 "
+        className="w-[70px] h-[70px] object-contain cursor-pointer fixed right-6 top-5 sm:right-8 sm:top-7 lg:right-[4rem] lg:top-[3rem] bg-slate-100 p-4 rounded-full"
         alt="nav menu"
         onClick={() => setToggle(!toggle)}
       />
@@ -19,31 +19,31 @@ const Navbar = () => {
       <ul
         className={
           toggle
-            ? "flex flex-col justify-center items-center gap-10 bg-red-200 h-[100vh] w-[100vw] z-10"
+            ? "grid justify-center items-center bg-gradient-to-br from-primaryDarkColor to-primaryLightColor h-[100vh] w-[100vw] z-10 py-[8rem] xs:text-[1.75rem] sm:text-[2rem]"
             : "hidden"
         }
       >
-        <li className="" onClick={() => setToggle(!toggle)}>
+        <li className="nav_link" onClick={() => setToggle(!toggle)}>
           <a href="#about" className="">
-            <span>01</span>About Natous
+            <span>01</span>About Lesnovo
           </a>
         </li>
-        <li className="" onClick={() => setToggle(!toggle)}>
+        <li className="nav_link" onClick={() => setToggle(!toggle)}>
           <a href="#features" className="">
             <span>02</span>Your benfits
           </a>
         </li>
-        <li className="" onClick={() => setToggle(!toggle)}>
+        <li className="nav_link" onClick={() => setToggle(!toggle)}>
           <a href="#tours" className="">
             <span>03</span>Popular tours
           </a>
         </li>
-        <li className="" onClick={() => setToggle(!toggle)}>
+        <li className="nav_link" onClick={() => setToggle(!toggle)}>
           <a href="#testimonials" className="">
             <span>04</span>Stories
           </a>
         </li>
-        <li className="" onClick={() => setToggle(!toggle)}>
+        <li className="nav_link" onClick={() => setToggle(!toggle)}>
           <a href="#contact" className="">
             <span>05</span>Book now
           </a>
