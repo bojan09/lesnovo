0dce80cae2b07046d58ee306b463e7d7f904f3ce
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-21 20:06:30 +0200

    updates in About, updates in assets folder

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import globe from "../assets/features/globe.png";
 import compas from "../assets/features/compas.png";
 import map from "../assets/features/map.png";
+import heart from "../assets/features/heart.png";
 
 const Features = () => {
   return <div></div>;
