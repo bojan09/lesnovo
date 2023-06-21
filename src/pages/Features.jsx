533cf225890831f28907d312e68e5ee3872f6a3d
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-21 17:16:40 +0200

    adding assets for Features page

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
+import globe from "../assets/features/globe.png";
+import compas from "../assets/features/compas.png";
+import map from "../assets/features/map.png";
+
 const Features = () => {
-  return <div>Features</div>;
+  return <div></div>;
 };
 
 export default Features;
