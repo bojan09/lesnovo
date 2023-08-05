b499fa2372a6609f7631bb50b70512f6fc048660
Bojan <bojan-stanimirovski097@hotmail.com>
2023-08-05 12:13:51 +0200

    final commit

--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Home } from "./Home";
-export { default as Features } from "./Features";
-export { default as About } from "./About";
-export { default as Tours } from "./Tours";
-export { default as Testimonials } from "./Testimonials";
-export { default as Contact } from "./Contact";
